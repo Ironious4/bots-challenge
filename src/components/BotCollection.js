1814a833f30ca6f622ae2b447ecec6d385b46156
Ironious4 <tyronekariuki@gmail.com>
2025-03-20 18:51:45 +0300

    deployment

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -5,7 +5,7 @@ function BotCollection({onAddBot, onBotDelete}) {
   const[bots, setBots]= useState([]); //State to store the list of all bots
 //useEffect hook to fetch bot data
  useEffect(()=> {
-  fetch('https://json-server-vercel-gamma-beryl.vercel.app/bots')
+  fetch('https://bots-db.vercel.app//bots')
   .then(response=>response.json())
   .then(data=> {
     setBots(data);
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -18,7 +18,7 @@ function BotsPage() {
   const handleBotDelete = async (botToDelete) => {
     try {
       // send delete request to the backend
-      await fetch(`https://json-server-vercel-gamma-beryl.vercel.app/bots/${botToDelete.id}`, {
+      await fetch(`https://bots-db.vercel.app//bots/${botToDelete.id}`, {
         method: "DELETE",
       });
       // Remove the bot from the army if it's in the army
